38314084dc0b87b7bd701a1555396943ab26592f
Kasper Schepers <513008@edu.rocmn.nl>
2018-11-02 11:15:36 +0100

    menu responsive

---
+++ menu.php
@@ -0,0 +1,60 @@
+<html>
+<head>
+    <link rel="stylesheet" type="text/css" href="menu.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+</head>
+
+<body>
+<div class="container">
+
+    <a class="navbar-brand logo"><img src="AFBEELDINGEN/logo.jpg"></a>
+    <nav class="navbar navbar-expand-md">
+        <button class="navbar-toggler" data-toggle="collapse" data-target="#collapse_target">
+            <span class="navbar-toggler-icon hamburger"></span>
+        </button>
+        <div class="row navigatiebalk">
+            <div class="collapse navbar-collapse" id="collapse_target">
+                <ul class="navbar-nav">
+                    <div class="col-xs-2 col-md-2 col-lg-2">
+                        <li class="nav-item">
+                            <a class="nav-link" href="index.php">HOME</a>
+                        </li>
+                    </div>
+                    <div class="col-xs-2 col-md-2 col-lg-2">
+                        <li class="nav-item">
+                            <a class="nav-link" href="Draagtassen.php">DRAAGTASSEN</a>
+                        </li>
+                    </div>
+                    <div class="col-xs-2 col-md-2 col-lg-2">
+                        <li class="nav-item">
+                            <a class="nav-link" href="Handvatten.php">HANDVATTEN</a>
+                        </li>
+                    </div>
+                    <div class="col-xs-2 col-md-2 col-lg-2">
+                        <li class="nav-item">
+                            <a class="nav-link" href="Over_ons.php">OVER ONS</a>
+                        </li>
+                    </div>
+                    <div class="col-xs-2 col-md-2 col-lg-2">
+                        <li class="nav-item">
+                            <a class="nav-link" href="Offerte.php">OFFERTE</a>
+                        </li>
+                    </div>
+                    <div class="col-xs-2 col-md-2 col-lg-2">
+                        <li class="nav-item">
+                            <a class="nav-link" href="Contact.php">CONTACT</a>
+                        </li>
+                    </div>
+                </ul>
+            </div>
+        </div>
+    </nav>
+    <div class="row gele_balk">
+        <div class="col-lg-12 balk"></div>
+    </div>
+</div>
+</body>
+</html>
